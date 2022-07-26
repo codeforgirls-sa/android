35bfeaa21bdbc64597fc0ec0534ebcdd6c1f6786
Basma-AlKhameri <basma21970@gmail.com>
2022-07-26 09:14:00 +0300

    edit

--- unit3/ZakatCalc2/app/src/main/java/com/example/vatcalc/MainActivity.java
+++ unit3/ZakatCalc2/app/src/main/java/com/example/vatcalc/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.vatcalc;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,35 +14,39 @@ public class MainActivity extends AppCompatActivity {
     EditText userInput;
     Button calc;
     TextView result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //init views
-        userInput=findViewById(R.id.userInput);
-        calc=findViewById(R.id.calcButton);
-        result=findViewById(R.id.result);
+        userInput = findViewById(R.id.userInput);
+        calc = findViewById(R.id.Calculate);
+        result = findViewById(R.id.result_value);
+
         /*calc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String userInputValue=userInput.getText().toString();
-                float inputAsNumber=0;
-                if (userInputValue.isEmpty())
-                    Toast.makeText(MainActivity.this,"الرجاء ادخال المبلغ" ,Toast.LENGTH_LONG).show();
-                else
-                   { inputAsNumber=Float.parseFloat(userInputValue);
-                result.setText((inputAsNumber/40)+"");}
-            }
-        });*/
-    }
-    public void onCalc(View view)
-    {
         String userInputValue=userInput.getText().toString();
-        float inputAsNumber=0;
+
         if (userInputValue.isEmpty())
             Toast.makeText(MainActivity.this,"الرجاء ادخال المبلغ" ,Toast.LENGTH_LONG).show();
         else
-        { inputAsNumber=Float.parseFloat(userInputValue);
+        {
+            Float inputAsNumber= Float.parseFloat(userInputValue);
         result.setText((inputAsNumber/40)+"");}
+            }
+        });*/
+    }
+
+    public void onCalc(View view) {
+        String userInputValue = userInput.getText().toString();
+
+        if (userInputValue.isEmpty())
+            Toast.makeText(MainActivity.this, "الرجاء ادخال المبلغ", Toast.LENGTH_LONG).show();
+        else {
+            Float inputAsNumber = Float.parseFloat(userInputValue);
+            result.setText((inputAsNumber / 40) + "");
+        }
     }
 }
