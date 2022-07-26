6bf997fbf2a1479e2ee159198532ef66c3e62e2b
Basma-AlKhameri <basma21970@gmail.com>
2022-07-26 17:00:50 +0300

    edit

--- unit3/GreetUser/app/src/main/java/com/example/greetuser/MainActivity.java
+++ unit3/GreetUser/app/src/main/java/com/example/greetuser/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         name=findViewById(R.id.userName);
         description=findViewById(R.id.description);
         save=findViewById(R.id.saveButton);
--- unit3/Profile2/app/src/main/java/com/example/profile/MainActivity.java
+++ unit3/Profile2/app/src/main/java/com/example/profile/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         name=findViewById(R.id.profileName);
         role=findViewById(R.id.profileRole);
         //get Data
